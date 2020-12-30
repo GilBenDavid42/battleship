bcd822080704b82b626fde916b775ee430d68664
GilBenDavid <giltalgil987@gmail.com>
2020-12-30 11:08:45 +0200

    Added error for bad position

--- errors/bad_position_error.py
+++ errors/bad_position_error.py
@@ -4,9 +4,11 @@ Purpose: bad_position_error.py
 Author: Gil Ben David
 Change Log: 12/29/2020 
 """
-import abc
 
 
-class Error(Exception):
+class BadPositionError(Exception):
+    def __init__(self, position):
+        self.position = position
+
     def __str__(self):
-        
+        return f"{self.position} is a bad position."
